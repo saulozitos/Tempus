43a26886acd8699fc19172ebf9253a1a45408780
Saulo Campos <contatosauloaraujo@gmail.com>
2021-03-25 14:32:40 -0300

    chronometro

--- main.cpp
+++ main.cpp
@@ -97,11 +97,90 @@ private:
     }
 };
 
+class Chronometer
+{
+private:
+    std::chrono::time_point<std::chrono::system_clock> m_StartTime{};
+    std::chrono::time_point<std::chrono::system_clock> m_EndTime{};
+    bool m_bRunning = false;
+
+public:
+    void start()
+    {
+        m_StartTime = std::chrono::system_clock::now();
+        m_bRunning = true;
+    }
+
+    void stop()
+    {
+        m_EndTime = std::chrono::system_clock::now();
+        m_bRunning = false;
+    }
+
+    double elapsedMilliseconds()
+    {
+        std::chrono::time_point<std::chrono::system_clock> endTime;
+
+        if(m_bRunning)
+        {
+            endTime = std::chrono::system_clock::now();
+        }
+        else
+        {
+            endTime = m_EndTime;
+        }
+
+        return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
+    }
+
+    double elapsedSeconds()
+    {
+        return elapsedMilliseconds() / 1000.0;
+    }
+
+    std::string getTimeStemp()
+    {
+        if(m_bRunning)
+            stop();
+
+        using namespace std::chrono;
+        using days = duration<int, std::ratio<86400>>;
+
+        nanoseconds ns = duration_cast<nanoseconds>(m_EndTime - m_StartTime);
+
+        auto d = duration_cast<days>(ns);
+        ns -= d;
+        auto h = duration_cast<hours>(ns);
+        ns -= h;
+        auto m = duration_cast<minutes>(ns);
+        ns -= m;
+        auto s = duration_cast<seconds>(ns);
+        ns -= s;
+        auto ms = duration_cast<milliseconds>(ns);
+
+        std::ostringstream os;
+        os << std::setfill('0') << std::setw(2) << d.count() << "d:"
+           << std::setfill('0') << std::setw(2) << h.count() << ":"
+           << std::setfill('0') << std::setw(2) << m.count() << ":"
+           << std::setfill('0') << std::setw(2) << s.count() << "."
+           << std::setfill('0') << std::setw(3) << ms.count();
+
+        return os.str();
+    }
+};
+
 int main()
 {
-    DateTime dt(DateTime::DateFormat::DDMMYYYY);
-    std::cout << dt.getCurrentDate() << std::endl;
-    std::cout << dt.getCurrentTime() << std::endl;
+    Chronometer c;
+    DateTime dt(DateTime::DateFormat::DDMMYY);
+
+    c.start();
     std::cout << dt.getCurrentDateTime() << std::endl;
+    while (c.elapsedSeconds() < 5.00) {
+    }
+
+    c.stop();
+    std::cout << dt.getCurrentDateTime() << std::endl;
+    std::cout << c.getTimeStemp() << std::endl;
     return 0;
 }
